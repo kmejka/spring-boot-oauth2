3741e26b3dd969ab85f39b08ee6ed4204323dd58
Krzysztof Mejka <krzysztof.mejka@gmail.com>
2018-03-15 23:13:39 +0100

    STEP-2 Customise oauth clients

---
+++ auth-server/src/main/java/com/kmejka/authserver/config/AuthorizationServerConfig.java
@@ -0,0 +1,61 @@
+package com.kmejka.authserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+
+@Configuration
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    private final static int TOKEN_EXPIRATION_SECONDS = 3600;
+
+    private AuthenticationManager authenticationManager;
+
+    public AuthorizationServerConfig(final AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.tokenStore(tokenStore())
+                .authenticationManager(authenticationManager)
+                .allowedTokenEndpointRequestMethods(GET, POST);
+    }
+
+    /**
+     * This method defines how secured are operations related to getting a new token and validating an existing token.
+     * This configuration is identical as adding the configuration parameters below:
+     * `security.oauth2.authorization.checkTokenAccess: isAuthenticated()` and
+     * `security.oauth2.authorization.tokenKeyAccess: permitAll()`
+     */
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) {
+        security.tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("acme")
+                .secret("acmesecret")
+                .accessTokenValiditySeconds(TOKEN_EXPIRATION_SECONDS)
+                .scopes("read", "write")
+//                .autoApprove()
+                .authorizedGrantTypes("client_credentials");
+    }
+}
